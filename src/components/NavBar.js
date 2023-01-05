2da9b883e52034c74c6e124fc533069f854184d5
Amine boutss <boutassmohammedamine@gmail.com>
2023-01-05 21:53:59 +0100

    assets-font

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { NavBar , Container } from"react-boostrap";
+import logo from '';
+
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState['home'];
     const [scrolled, seScrolled] = useState[false];
