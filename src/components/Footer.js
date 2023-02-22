45b62abb211714f97bd1a81b5b3f94eb0c7b829b
Amine boutss <boutassmohammedamine@gmail.com>
2023-02-22 02:48:34 +0100

    sgg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,20 +10,37 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            {/*<div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-  </div>*/}
+            <div className="social-icon">
+              <a
+                href="https://www.linkedin.com/in/mohammed-amine-boutass-b628901b9/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a
+                href="https://web.facebook.com/sanawnawz"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              <a
+                href="https://www.instagram.com/amimeboutass/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={navIcon3} alt="Icon" />
+              </a>
+            </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
